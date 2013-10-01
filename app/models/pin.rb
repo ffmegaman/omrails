d10f61a83c599707ee8a47b639409688370260e9
Philip Q Nguyen <supertaru@gmail.com>
2013-10-01 19:23:30 +0900

    add image loading by URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
 
 
   validates :description, presence: true
@@ -7,10 +7,14 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, styles: { medium: "320x240" }
 	validates_attachment :image, presence: true,
-	content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-	size: { less_than: 5.megabytes }
+																content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+																size: { less_than: 5.megabytes }
 
   belongs_to :user
   
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
   
 end
